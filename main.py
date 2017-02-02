eb51f0031466889a324f4f650af93db51d605d48
Tim Bric <TJBric@gmail.com>
2017-02-01 21:01:29 -0500

    Put finishing touches onto caesar and escaped HTML

--- main.py
+++ main.py
@@ -16,19 +16,39 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content):
+    rot_label = "<label>Rotate by: </label>"
+    rotation_input = "<input name='rot' type='number'>"
+
+    message_label ="<label>Type a message: </label>"
+    textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+
+    submit = "<input type='submit'/>"
+    form = ("<form method='post'>" +
+            message_label + "<br>" + textarea + "<br>"
+            + rot_label + "<br>" + rotation_input + "<br>"
+            + submit + "</form>")
+
+    header = "<h2>Web Caesar</h2>"
+
+    return header + form
+
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        content = build_page("")
+        self.response.write(content)
 
-        message = "Sup, world!"
-        encrypted_message = caesar.encrypt(message, 13)
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
-        submit = "<input type='submit'/>"
-        form = "<form>" + textarea + "<br>" + submit + "</form>"
-        self.response.write(form)
-
-
+    def post(self):
+        message = self.request.get("message")
+        rot = int(self.request.get("rot"))
+        encrypted_message = caesar.encrypt(message,rot)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
+        self.response.write(content)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
